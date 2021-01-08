87214c5a02713e1d8387e33f603e490f0ef073e7
Nik Sofianos <nsofianos@hotmail.com>
2021-01-08 08:58:27 +0000

    add basic http authentication

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with :name  => ENV['ADMIN_USERNAME'], :password => ENV['ADMIN_PASSWORD']
+
   def index
     @products = Product.order(id: :desc).all
   end
@@ -36,5 +38,6 @@ class Admin::ProductsController < ApplicationController
       :price
     )
   end
+
 
 end
